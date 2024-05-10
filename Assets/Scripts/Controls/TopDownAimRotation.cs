4c68b8badf84a25bf705f8b11737aff92133c309
201870024YuSeungHyeon <jyyootom@naver.com>
2024-05-10 19:44:46 +0900

    프로젝트 생성 -> 캐릭터 이동까지

---
+++ Assets/Scripts/Controls/TopDownAimRotation.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 newAimDirection)
+    {
+        RotateChar(newAimDirection);
+    }
+
+    private void RotateChar(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+}
